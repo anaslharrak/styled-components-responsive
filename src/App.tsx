e224039c3fa14372e15694a2edf6e44ccf944ce3
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2023-11-13 17:00:31 +0100

    added css responsive with SC

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import exampleImage from './assets/example.jpg'
+import styled from 'styled-components'
+
+const size = {
+  mobilGrande: '640px',
+  tablet: '768px',
+  pc: '1024px',
+  pcXL: '1280px',
+  pc2XL: '1536px'
+}
+
+const device = {
+  mobilGrande: `(min-width: ${size.mobilGrande})`,
+  tablet: `(min-width: ${size.tablet})`,
+  pc: `(min-width: ${size.pc})`,
+  pcXL: `(min-width: ${size.pcXL})`,
+  pc2XL6: `(min-width: ${size.pc2XL})`
+}
+
+const Section = styled.section`
+  display: grid;
+  gap: 2rem;
+  background-color: #34D399;
+  text-align: center;
+  border-radius: 0.5rem;
+
+  @media ${device.tablet} {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    align-items: center;
+    text-align: left;
+    background-color: #FB923C;
+    padding: 2.5%;
+  }
+
+  @media ${device.pc} {
+    background-color: #60A5FA;
+  }
+`
+
+const ResponsiveImage = styled.img`
+  width: 100%;
+  border-radius: 0.5rem;
+`
+
+const Title = styled.h1`
+  margin-bottom: 0.5rem;
+  font-size: 2.25rem;
+  font-weight: 500;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Section>
+        <div>
+          <ResponsiveImage src={exampleImage} alt="Responsive image"/>
+        </div>
+
+        <div>
+          <Title>Headline</Title>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptatibus cum quidem odio dicta ipsam nesciunt earum numquam ex nulla facere. 
+            Laborum est qui quasi animi cum alias tempore, sint ut.</p>
+        </div>
+      </Section>  
     </>
   )
 }
